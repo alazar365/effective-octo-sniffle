d18b81b2c3cf7054e59b92458ab377fabe9a3607
Alexandru Lazar <79793893+alazar-HELB@users.noreply.github.com>
2024-01-15 14:47:22 +0100

    add Customer.java + postgresql configuration

--- src/main/java/com/alexspring/Main.java
+++ src/main/java/com/alexspring/Main.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class Main {
@@ -12,12 +14,25 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
 
+//    @GetMapping("/greet")
+//    public GreetResponse greet(){
+//        return new GreetResponse("Hello");
+//    }
+
     @GetMapping("/greet")
-    public GreetResponse greet(){
-        return new GreetResponse("Hello");
+    public GreetResponseRecord greet(){
+        return new GreetResponseRecord(
+                "Hello",
+                List.of("Java", "C#", "Python"),
+                new Person("Alex", 28, 30_000));
     }
 
-    record Person (String name){
+    record Person (String name, int age, double savings){
 
     }
+
+    record GreetResponseRecord(
+                String greet,
+                List<String> favProgrammingLanguages,
+                Person person) {}
 }
