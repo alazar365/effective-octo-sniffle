de6cbfd7e8b8693f319172a5a725a5073f6ad4e1
Alexandru Lazar <79793893+alazar-HELB@users.noreply.github.com>
2024-01-18 14:17:23 +0100

    endpoint for reading API

--- src/main/java/com/alexspring/Main.java
+++ src/main/java/com/alexspring/Main.java
@@ -3,13 +3,22 @@ package com.alexspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
@@ -18,6 +27,11 @@ public class Main {
 //    public GreetResponse greet(){
 //        return new GreetResponse("Hello");
 //    }
+
+    @GetMapping
+    public List<Customer> getCustomers(){
+        return customerRepository.findAll();
+    }
 
     @GetMapping("/greet")
     public GreetResponseRecord greet(){
