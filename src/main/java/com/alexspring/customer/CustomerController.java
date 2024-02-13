f2289acf9273399c3ce4f19187e6efc538cd4f2e
Alexandru Lazar <79793893+alazar-HELB@users.noreply.github.com>
2024-02-13 12:20:34 +0100

    quick change

--- src/main/java/com/alexspring/customer/CustomerController.java
+++ src/main/java/com/alexspring/customer/CustomerController.java
@@ -1,6 +1,5 @@
 package com.alexspring.customer;
 
-import com.alexspring.customer.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
